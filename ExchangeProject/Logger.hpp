3d0609446d51e50f97d9b4799d68faa8e5689820
infrader <happe112fi@gmail.com>
2026-01-07 07:54:40 +0300

    Добавил простые макросы

--- ExchangeProject/Logger.hpp
+++ ExchangeProject/Logger.hpp
@@ -3,6 +3,17 @@
 #include<iostream>
 #include<fstream>
 
+//===================Макросы==========================
+#define Log_Intence Logger::GetIntence
+#define Log_Info(msg) Logger::GetIntence().info(msg)
+#define Log_Trace(msg) Logger::GetIntence().trace(msg)
+#define Log_Debug(msg) Logger::GetIntence().debug(msg)
+#define Log_Critical(msg) Logger::GetIntence().critical(msg)
+#define Log_Warn(msg) Logger::GetIntence().warn(msg)
+#define Log_Error(msg) Logger::GetIntence().error(msg)
+#define Log_SetLoggerFile(msg) Logger::GetIntence().setLogger_file(msg)
+//====================================================
+
 class Logger {
 public:
 	enum class State_Level {INFO,TRACE,DEBUG,CRITICAL,WARN,ERROR};
