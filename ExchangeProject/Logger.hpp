5f9481ce1e5855a438da8f8c47f1d48d4cdd58d4
infrader <happe112fi@gmail.com>
2026-01-07 07:26:36 +0300

    Добавьте файлы проекта.

---
+++ ExchangeProject/Logger.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include<string>
+#include<iostream>
+#include<fstream>
+
+class Logger {
+public:
+	enum class State_Level {INFO,TRACE,DEBUG,CRITICAL,WARN,ERROR};
+	static Logger& GetIntence();
+	 Logger(const Logger&) = delete;
+	 Logger& operator=(const Logger&) = delete;
+	 void setLogger_file(std::string_view msg);
+	 void info(std::string_view msg);
+	 void trace(std::string_view msg);
+	 void debug(std::string_view msg);
+	 void critical(std::string_view msg);
+	 void warn(std::string_view msg);
+	 void error(std::string_view msg);
+	 ~Logger() = default;
+private:
+	Logger() { std::cout << "\nLgger/file успешно создан!\n"; };
+	void log(State_Level level, std::string_view msg);
+};
---
+++ ExchangeProject/main.cpp
@@ -0,0 +1,8 @@
+﻿#include<iostream>
+#include <cpr/cpr.h>
+
+int main() {			// Главная функция main
+	
+	std::cout << "Hello\n";
+	std::cin.get();
+}
