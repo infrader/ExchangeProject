a46f01d5f8725fd76b5f5753b2c153aee090587b
infrader <happe112fi@gmail.com>
2026-01-13 19:37:42 +0300

    Усложнил логику использую двойную буфферизацию. Буду переписывать на атомарность

--- ExchangeProject/Exchange.cpp
+++ ExchangeProject/Exchange.cpp
@@ -1,25 +1,53 @@
 #include "Exchange.hpp"
-std::chrono::seconds Exchange::caсhe::update_period = 4s;
-void Exchange::get_all_tickers(){
-	
+std::chrono::seconds Exchange::cache::update_period = 4s;
+
+void Exchange::set_api(std::string api_name){
+	this->Api = api_name;
+}
+std::string Exchange::get_api() {
+	return this->Api;
 }
 
-void Exchange::get_token()	{
 
-}
-
-std::unordered_map<std::string, TokenInfo> Exchange::excange_cache(){
-	if (!expired_cache()) {
-		return cashe_map.data;
+std::unordered_map<std::string, TokenInfo> Exchange::double_buffer(){
+	expired_cache(cache_B);
+	expired_cache(cache_A);
+	if (cache_A.state == FRESH && cache_B.state == FRESH) {
+		return cache_A.data_buffer;
 	}
-	else {
+	else if (cache_A.state == EXPIRED && cache_B.state == EXPIRED) {
+		std::thread upload_cache([&]() {Exchange::excange_cache(cache_B);});
+		return cache_A.data_buffer;
+	}
+	else if (cache_A.state == UNLOADING && cache_B.state == EXPIRED) {
+		return cache_B.data_buffer;
+	}
+	else if (cache_A.state == EXPIRED && cache_B.state == UNLOADING) {
+		return cache_A.data_buffer;
+	}
+	else if (cache_A.state == FRESH && cache_B.state == EXPIRED || cache_B.state == UNLOADING) {
+		std::thread upload_cache([&]() {cache_B = cache_A; });
+		return cache_A.data_buffer;
+	} 
+	else if (cache_B.state == FRESH && cache_A.state == EXPIRED || cache_A.state == UNLOADING) {
+		std::thread upload_cache([&]() {cache_A = cache_B; });
+		return cache_B.data_buffer;
+	}
+}
+
+void Exchange::excange_cache(cache& cache){
+	load_lock.lock();
 		try {
+			cache.state = cache_state::UNLOADING;
 			exchange_session.SetUrl(cpr::Url(Api));
 			exchange_session.SetTimeout({ 300 });
 			exchange_response = exchange_session.Get();
 			if (exchange_response.status_code != 200) {
 				throw std::runtime_error("Api status isn`t 200!");
 			}
+			cache.data_buffer = parse(exchange_response);
+			cache.last_update = std::chrono::steady_clock::now();
+			cache.state = cache_state::FRESH;
 		}
 		catch (const std::runtime_error& er) {
 			Log_Warn(er.what());
@@ -27,24 +55,26 @@ std::unordered_map<std::string, TokenInfo> Exchange::excange_cache(){
 		catch (...) {
 
 		}
+	load_lock.unlock();
 	}
-}
 
-bool Exchange::expired_cache()
+void Exchange::expired_cache(cache& cache)
 {
 	try {
-		if (cashe_map.last_update == std::chrono::system_clock::time_point{}) {
-			return true;
+		if (cache_A.data_buffer.size() == 0 && cache_B.data_buffer.size() == 0) {
+			excange_cache(cache_A);
+			cache_B.data_buffer = cache_A.data_buffer;
 		}
-		auto expired_time = cashe_map.last_update + Exchange::caсhe::update_period;
-		if (std::chrono::system_clock::now() > expired_time) {
-			return true;
-		}
+		auto expired_time = cache.last_update + Exchange::cache::update_period;
+		if (std::chrono::steady_clock::now() > expired_time) {
+			cache.state = cache_state::EXPIRED;
+			}
 		else {
-			return false;
+			cache.state = cache_state::FRESH;
 		}
 	}
 	catch (...) {
 		Log_Critical("Критическая ошибка с определением expired_cache!");
 	}
 }
+
--- ExchangeProject/Exchange.hpp
+++ ExchangeProject/Exchange.hpp
@@ -6,31 +6,38 @@
 #include<cpr/cpr.h>
 #include<chrono>
 #include"Logger.hpp"
+#include<memory>
+#include<thread>
+#include<mutex>
 using namespace std::chrono_literals;
 struct TokenInfo{
 	std::string symbolpair;
 	double price_of_sell;
-	double price_of_vol;
+	double vol;
 	double price_of_buy;
 };
 class Exchange{
 public:
-	void get_all_tickers();
-	void set_api();
-	void get_api(); 
-	void get_token();
+	void set_api(std::string api_name);
+	std::string get_api(); 
+	TokenInfo get_token(std::string symbol);
 private:
-	struct cañhe {
+	std::mutex load_lock;
+	enum cache_state { FRESH, EXPIRED, UNLOADING };
+	struct cache {
 	public:
-		std::chrono::system_clock::time_point last_update;
+		cache_state state;
+		std::chrono::steady_clock::time_point last_update;
 		static std::chrono::seconds update_period;
-		std::unordered_map<std::string, TokenInfo> data;
+		std::unordered_map<std::string, TokenInfo> data_buffer;
 	};
+	std::unordered_map<std::string, TokenInfo> double_buffer();
 	std::string Api;
-	cañhe cashe_map;
-	std::unordered_map<std::string, TokenInfo> excange_cache();
-	bool expired_cache();
-	virtual void parse() = 0;
+	cache cache_B;
+	cache cache_A;
+	void excange_cache(cache& cache);
+	void expired_cache(cache& cache);
+	virtual std::unordered_map<std::string, TokenInfo> parse(cpr::Response exchange_response) = 0;
 	cpr::Session exchange_session;
 	cpr::Response exchange_response;
 };
