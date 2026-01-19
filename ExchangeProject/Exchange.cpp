5dc80a4bca22a8308b68f78ab49a56b8c1b3fb2c
infrader <happe112fi@gmail.com>
2026-01-19 12:00:53 +0300

    Атомарный паттерн фонового обновления кэша с однопоточным писателем и многопоточными читателями

--- ExchangeProject/Exchange.cpp
+++ ExchangeProject/Exchange.cpp
@@ -1,5 +1,4 @@
 #include "Exchange.hpp"
-std::chrono::seconds Exchange::cache::update_period = 4s;
 
 void Exchange::set_api(std::string api_name){
 	this->Api = api_name;
@@ -8,33 +7,38 @@ std::string Exchange::get_api() {
 	return this->Api;
 }
 
-Exchange::~Exchange(){
-	while (lock_update.load()) {
-		std::this_thread::yield();
-	}
+void Exchange::spin_upload_start() // Мы запускаем в другом потоке spin_lock
+{
+	uploading_state.store(true);
+	uploading_data();
 }
 
-void Exchange::excange_cache(cache& cache){
-	std::lock_guard<std::mutex> cache_lc(load_lock);
-		try {
-			cache.state = cache_state::UNLOADING;
-			exchange_session.SetUrl(cpr::Url(Api));
-			exchange_session.SetTimeout({ 300 });
-			exchange_response = exchange_session.Get();
-			if (exchange_response.status_code != 200) {
-				throw std::runtime_error("Api status isn`t 200!");
-			}
+void Exchange::spin_upload_stop() // Перед тем как за джоинить upload_start - нужно обязательно выполнить остановку upload_stop().
+{
+	uploading_state.store(false);
+}
 
-		}
-		catch (const std::runtime_error& er) {
-			Log_Warn(er.what());
-			cache.state = cache_state::EXPIRED;
-			this->lock_update = false;
-		}
-		catch (...) {
-			Log_Critical("Неизвестная ошибка excange_cache");
-			cache.state = cache_state::EXPIRED;
-			this->lock_update = false;
+void Exchange::uploading_data(){ // 1 писатель не беcпокоимся что здесь будет 2 и более потока!
+	try {
+		exchange_session.SetUrl(cpr::Url{ Api });
+		exchange_session.SetTimeout({ 300 });
+		while (uploading_state) {
+			while (time_now <= upload_time && data_upload_count != 0) {
+				std::this_thread::yield();
+			}
+			exchange_response = exchange_session.Get();
+			time_now = std::chrono::steady_clock::now();
+			data_upload = parse(exchange_response);
+			std::atomic_signal_fence(std::memory_order_seq_cst); // Барьер set_cst чтобы время мы получили после Get точно и компилятор не путаялся
+			std::unique_lock<std::mutex> guard(move_data); // Мютекс флаг который говорит перестать работать с data_cache будеь свап
+			data_cache = std::move(data_upload);
+			uploud_succses.notify_all();
+			upload_time = time_now + std::chrono::milliseconds(3000);
+			data_upload_count++;
 		}
 	}
+	catch (...) {
+
+	}
+}
 
--- ExchangeProject/Exchange.hpp
+++ ExchangeProject/Exchange.hpp
@@ -10,7 +10,6 @@
 #include<thread>
 #include <atomic>
 #include<mutex>
-using namespace std::chrono_literals;
 struct TokenInfo{
 	std::string symbolpair;
 	double price_of_sell;
@@ -19,26 +18,22 @@ struct TokenInfo{
 };
 class Exchange{
 public:
+	Exchange() :data_upload_count(0) {};
 	void set_api(std::string api_name);
 	std::string get_api(); 
-	~Exchange();
+	void spin_upload_start(); // Включить обновление данных
+	void spin_upload_stop(); // Выключить обновление данных
 private:
-	std::atomic<bool> lock_update{false};
-	std::mutex update_mtx;
-	std::mutex load_lock;
-	enum cache_state { FRESH, EXPIRED, UNLOADING };
-	struct cache {
-	public:
-		std::atomic<cache_state> state;
-		std::chrono::steady_clock::time_point last_update;
-		static std::chrono::seconds update_period;
-		std::unordered_map<std::string, TokenInfo> data_buffer;
-	};
-	std::atomic<cache*> active_cache{ &cache_A };
+	std::atomic<bool> uploading_state{true}; // Включить - выключить обновление данных!
+	std::chrono::steady_clock::time_point time_now;
+	std::chrono::steady_clock::time_point upload_time;
+	int data_upload_count; // Сколько циклов обновления данных прошли
+	void uploading_data();
+	std::mutex move_data; // Мютекс который защищает всех читателей в момент когда у нас данные муваются 
+	std::condition_variable uploud_succses; // -Метка готовы ли наши данные или нет из api!
+	std::unordered_map<std::string, TokenInfo> data_upload; // обновление в фоне/ move(data_upload)
+	std::unordered_map<std::string, TokenInfo> data_cache; // - то чем мы работаем, 
 	std::string Api;
-	cache cache_B;
-	cache cache_A;
-	void excange_cache(cache& cache);
 	virtual std::unordered_map<std::string, TokenInfo> parse(cpr::Response exchange_response) = 0;
 	cpr::Session exchange_session;
 	cpr::Response exchange_response;
